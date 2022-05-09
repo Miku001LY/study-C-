80479622dd4a8dfaadc536a9a94b1991f7402dba
Miku001LY <101001143+Miku001LY@users.noreply.github.com>
2022-05-09 13:45:34 +0800

    Add files via upload
    第一次作业

---
+++ 22-5-9-0.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+
+//int main()
+//{
+//	//字符串的结束标志'\0'
+//	// "abcdef";
+//	//'\0'-转义字符-0
+//	//0->数字0
+//	//'0'->字符0-数字字符--ASCII值48
+// //EOF-end of file-文件结束标志--  -1
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	if (num1 > num2)
+//		printf("较大值=%d\n", num1);
+//	else
+//		printf("较大值=%d\n", num2);
+//	return 0;
+//}
+
+//int Max(int x, int y)//自定义函数--接收
+//{
+//	if (x > y)
+//		return x;//返回值x--返回类型整形
+//	else
+//		return y;
+//}
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	int max = 0;
+//	max=Max(num1, num2);//Max帮我找到num1和num2的较大值--通过返回值的方式告诉我值--将数据传上去
+//	printf("max=%d\n", max);
+//	return 0;
+//}
